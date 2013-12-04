4df641154db7535888a049ccbee15bf088191a8f
Kyle <kyle@doubledutch.me>
2013-12-03 18:19:37 -0800

    adding first 2 and some lessons from javascript foundations, through null and undefined

---
+++ myscript.js
@@ -0,0 +1,23 @@
+//variable lesson
+
+/*
+var color = "red";
+
+var myDiv = document.getElementByID('myDiv')
+myDiv.style.background = "black";
+myDiv.style.color = "#ffffff";
+*/
+
+//null and undefined lesson
+var myVar;
+
+//undefined = true;
+
+console.log(typeof myVar === "undefined");
+console.log(myVar === "undefined");
+
+if(myVar == null){
+	console.log("If");
+} else {
+	console.log("Else");
+}
