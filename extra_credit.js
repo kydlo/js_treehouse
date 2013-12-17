a2b8836ff494053b9b05b664757ed6511673176e
Kyle <kyle@doubledutch.me>
2013-12-16 17:28:04 -0800

    updating extra_credit.js

---
+++ extra_credit.js
@@ -0,0 +1,11 @@
+for(var counter=100; counter; counter = counter -1){
+
+	if (num_a%2){
+		return "even";
+	}
+	else 
+		return "odd";
+
+
+var starting_number = prompt("What number shall we start with?")
+	console.log ("Initial Number is")
