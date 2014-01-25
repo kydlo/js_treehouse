02de7f4b6f4d55afa214b0a78277067139571b01
Kyle <kyle@doubledutch.me>
2014-01-24 17:10:30 -0800

    added 2 extra statements from 1st lesson

--- strings.js
+++ strings.js
@@ -4,3 +4,11 @@ var name = "Kyle";
 console.log(name);
 
 var name2 = 'Kyle too'
+console.log(name2)
+
+var statement = "This is Kyle's string";
+console.log(statement)
+
+var statement2 = 'He said "This is awesome"';
+console.log(statement2)
+
